2c7e45e0f272c1596676d3e9f43eb6b04f62c31d
newall <newalljemuel.d@gmail.com>
2020-10-24 12:03:29 +0530

    Setup the project and finish sidebar section

---
+++ src/Feed.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Feed() {
+    return (
+        <div>
+        {/* Header  */}
+        <h2>This is the home</h2>
+
+
+        {/* TweetBox */}
+
+
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+            
+        </div>
+    )
+}
+
+export default Feed
