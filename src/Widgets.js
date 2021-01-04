0b2be08251d879aa53aef6d09ae0b970bab11768
newall <newalljemuel.d@gmail.com>
2021-01-04 13:10:56 +0530

    Style the widgets section

--- src/Widgets.js
+++ src/Widgets.js
@@ -10,8 +10,8 @@ import SearchIcon from "@material-ui/icons/Search";
 function Widgets() {
     return (
         <div className="widgets">
-            <div className="widgets__input">
-                <SearchIcon className="widgets__searchIcon" />
+            <div className="widgets_input">
+                <SearchIcon className="widgets_searchIcon" />
                 <input placeholder="Search Twitter" type="text" />
             </div>
 
